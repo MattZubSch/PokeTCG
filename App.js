d1b74cbb7f6090604f648929fa993eda73962362
Matias Zuber Schachner <matias.zs.1967@gmail.com>
2023-09-26 04:41:39 -0300

    Clase 3 - Core Components

--- App.js
+++ App.js
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Modal } from 'react-native';
+import React from 'react';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput 
+        placeholder="Item de Lista" 
+        style={styles.input} 
+        />
+        <Button title="AGREGAR" />
+      </View>
+      <View style={styles.itemList}>
+        {/* Lista de Items */}
+        <View style={styles.item}>
+          <Text>Item 1</Text>
+          <Button
+            title="X"
+            color={"red"}
+          />
+        </View>
+        <View style={styles.item}>
+          <Text>Item 2</Text>
+          <Button
+            title="X"
+            color={"red"}
+          />
+        </View>
+        <View style={styles.item}>
+          <Text>Item 3</Text>
+          <Button
+            title="X"
+            color={"red"}
+          />
+
+        </View>    
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 30
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between", 
+    alignItems: "center",
+    marginVertical: 30
+  },
+  input: {
+    borderBottomColor: "black", 
+    borderBottomWidth: 1,
+    width: 200
+  },
+  item: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  itemList: {
+    width: "100%",
+    borderColor : "black",
+    borderWidth: 1,
+    padding: 10,
+    marginVertical: 30  }
 });
